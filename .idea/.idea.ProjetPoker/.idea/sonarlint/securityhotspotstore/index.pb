b86f96099ba0344565dd9cc5efe977c9af1173e8
GrosJambon1243 <kevindurocher@live.ca>
2024-04-12 16:30:32 -0400

    Game State + PlayerAction + DeActirnvate Cursor Opp tu

--- .idea/.idea.ProjetPoker/.idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/.idea.ProjetPoker/.idea/sonarlint/securityhotspotstore/index.pb
@@ -5,11 +5,5 @@ K
 Assets/Script/Calculator.cs,b\3\b3d16f8163b64c88089590d56fc670e7cc6efc65
 K
 Assets/Script/TextScript.cs,7\e\7e950ee0a9f58ca7082646806d0b4a452ebb8855
-R
-"Assets/Script/State/PlayerState.cs,5\f\5f978d4245edf6221287dabac53baf251f8e2658
-P
- Assets/Script/State/GameState.cs,c\9\c9271aec16c252dfc765745fe48d757be259404e
-S
-#Assets/Script/State/StateManager.cs,b\a\baa2c368fdc61ed62fb88cf93799ce25bb210e0c
 W
 'Assets/Script/State/GameStateManager.cs,c\e\cec1e0d6ca171e73b190d474380d1e34e34b3fef
--- Assets/Script/State/GameStateManager.cs
+++ Assets/Script/State/GameStateManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum RoundState
 {
@@ -13,32 +14,170 @@ public enum RoundState
 }
 public class GameStateManager : MonoBehaviour
 {
-    public RoundState currentState;
+    [SerializeField] public Button drawButton;
+    public RoundStateBase currentState;
+    public RoundStateBase[] allState = new RoundStateBase[5];
+   
 
     private void Start()
     {
-        currentState = RoundState.PlayerTurn;
+        allState[0] = new PlayerTurnState(this);
+        allState[1] = new OpponentTurnState(this);
+        allState[2] = new EndOfRoundState(this);
+        allState[3] = new ShoppingState(this);
+        allState[4] = new GameOverState(this);
+
+        currentState = allState[0];
+        currentState.OnStateEnter();
     }
 
     private void Update()
     {
-        
+        currentState.UpdateState();
+    }
+
+    public void OnDrawCLick()
+    {
+        currentState.OnDrawClick();
+        if (currentState.PlayerAction == 0)
+        {
+            drawButton.enabled = false;
+        }
+       
+    }
+
+    public void OnEndTurnClick()
+    {
+        currentState.OnEndTurnClick();
     }
 
     public void TransitionToState(RoundState nextState)
     {
-        currentState = nextState;
+        currentState = allState[(int)nextState];
+        currentState.OnStateEnter();
     }
 }
 
 public abstract class RoundStateBase
 {
     protected GameStateManager gameStateManager;
+    private int playerAction;
+    public int PlayerAction
+    {
+        get => playerAction;
+        set => playerAction = value;
+    }
 
     public RoundStateBase(GameStateManager gameStateManager)
     {
         this.gameStateManager = gameStateManager;
     }
+    public virtual void OnDrawClick()
+    {
+        Debug.Log("Cannot Draw");
+    }
 
+    public virtual void OnEndTurnClick()
+    {
+        
+    }
+
+    public abstract void OnStateEnter();
+    
+        
+    
     public abstract void UpdateState();
+}
+
+public class PlayerTurnState : RoundStateBase
+{
+   
+    public PlayerTurnState(GameStateManager gameStateManager) : base(gameStateManager)
+    {
+    }
+
+    public override void OnStateEnter()
+    {
+        
+        PlayerAction = 2;
+    }
+
+    public override void UpdateState()
+    {
+        
+    }
+
+    public override void OnDrawClick()
+    {
+       Debug.Log("Drawing");
+       PlayerAction--;
+       
+    }
+
+    public override void OnEndTurnClick()
+    {
+        gameStateManager.TransitionToState(RoundState.OpponentTurn);
+    }
+}
+public class OpponentTurnState : RoundStateBase
+{
+    public OpponentTurnState(GameStateManager gameStateManager) : base(gameStateManager) { }
+
+    public override void OnStateEnter()
+    {
+        Cursor.visible = false;
+        Debug.Log("Opponnent Turn");
+    }
+
+    public override void UpdateState()
+    {
+       
+    }
+}
+public class EndOfRoundState : RoundStateBase
+{
+    public EndOfRoundState(GameStateManager gameStateManager) : base(gameStateManager)
+    {
+    }
+
+    public override void OnStateEnter()
+    {
+        
+    }
+
+    public override void UpdateState()
+    {
+       
+    }
+}
+
+public class GameOverState : RoundStateBase
+{
+    public GameOverState(GameStateManager gameStateManager) : base(gameStateManager) { }
+
+    public override void OnStateEnter()
+    {
+        
+    }
+
+    public override void UpdateState()
+    {
+      
+    }
+}
+public class ShoppingState : RoundStateBase
+{
+    public ShoppingState(GameStateManager gameStateManager) : base(gameStateManager)
+    {
+    }
+
+    public override void OnStateEnter()
+    {
+        
+    }
+
+    public override void UpdateState()
+    {
+        
+    }
 }
